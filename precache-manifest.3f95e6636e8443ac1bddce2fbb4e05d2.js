bb9bbbd2a6b71d66d282689c77fa867c7b916c4d
satyaprakashmohanty13 <mohanty.subash1984@gmail.com>
2025-07-31 10:21:29 +0530

    Add files via upload

---
+++ precache-manifest.3f95e6636e8443ac1bddce2fbb4e05d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b90536d980ea01f65d185256e1cb50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3ad78c680182795936",
+    "url": "/static/css/main.9499ae93.chunk.css"
+  },
+  {
+    "revision": "507f7f772c60cf13d951",
+    "url": "/static/js/2.f1c34aac.chunk.js"
+  },
+  {
+    "revision": "3fee6343beae956928494854364befaf",
+    "url": "/static/js/2.f1c34aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3ad78c680182795936",
+    "url": "/static/js/main.e1c11519.chunk.js"
+  },
+  {
+    "revision": "0607b5ce656469f45f78",
+    "url": "/static/js/runtime-main.cee0cca5.js"
+  }
+]);
